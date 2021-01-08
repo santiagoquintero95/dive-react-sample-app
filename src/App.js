dc0641febade86139f9107d22613cb825ec19288
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-01-08 13:33:50 -0600

    update before pushing ti to the repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './style.css'
+import IDVC from '@idscan/idvc'
+import  '@idscan/idvc/dist/css/idvc.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+    publicKey = 'REPLACE_ME';
+    backendServerUrl = '"REPLACE_ME"';
+    licenseKey = 'REPLACE_ME';
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            component: null
+        }
+    }
+
+    componentWillUnmount() {
+        this.state.component.stopProccesing()
+    }
+
+    componentDidMount () {
+        let _t = this
+        if (!this.state.component) {
+            this.state.component = new IDVC ({
+                networkUrl: 'networks',
+                el: 'videoCapturingEl',
+                licenseKey: this.licenseKey,
+                types: ['ID'],
+                showSubmitBtn: true,
+                steps: [
+                    {type: 'front', name: 'Front Scan'},
+                    {type: 'back', name: 'Back Scan'},
+                    {type: 'face', name: 'Selfie'}
+                ],
+                onChange (step) {
+                    console.log(step)
+                },
+                onReset (steps) {
+                    console.log(steps)
+                },
+                submit (data) {
+                    let backStep = data.steps.find(item => item.type === 'back')
+                    let trackString = (backStep && backStep.trackString) ? backStep.trackString : ''
+
+                    let request = {
+                        frontImageBase64: data.steps.find (item => item.type === 'front').img.split (/:image\/(jpeg|png);base64,/)[2],
+                        backOrSecondImageBase64: backStep.img.split (/:image\/(jpeg|png);base64,/)[2],
+                        faceImageBase64: data.steps.find (item => item.type === 'face').img.split (/:image\/(jpeg|png);base64,/)[2],
+                        documentType: data.documentType,
+                        trackString: trackString
+                    }
+                    _t.setState({isLoading: true})
+                    fetch ('https://dvs2.idware.net/api/Request', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8',
+                            'Authorization': `Bearer ${this.publicKey}`
+                        },
+                        body: JSON.stringify (request)
+                    }).then (response => response.json ())
+                        .then (response => {
+                            fetch ( this.backendServerUrl + '/api/ValidationRequests/complete/' , {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json;charset=utf-8'
+                                },
+                                body: JSON.stringify ({
+                                    requestId: response.requestId,
+                                    documentType: response.documentType
+                                })
+                            }).then (response => response.json ())
+                                .then (data => {
+                                    _t.setState({isLoading: false})
+                                    alert((data.payload.isDocumentSuccess) ? 'Document valid' : 'Document invalid')
+                                })
+                        }).catch(() => {
+                        _t.setState({isLoading: false})
+                    })
+                }
+            })
+        }
+    }
+    
+    render () {
+        const isLoading = this.state.isLoading
+        return (
+            <div>
+                {
+                    !isLoading
+                        ? null
+                        : (
+                            <div className="loading">Verify...</div>
+                        )
+                }
+                <h3>DVS Demo Application</h3>
+                <div id="videoCapturingEl"></div>
+            </div>
+        );
+    }
 }
 
 export default App;
